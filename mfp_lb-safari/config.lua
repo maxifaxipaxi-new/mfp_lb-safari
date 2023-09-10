c1dcc38b6aad645bc7b993ccc9297374bb1e78a5
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2023-09-10 17:43:58 +0200

    V. 1.2

--- mfp_lb-safari/config.lua
+++ mfp_lb-safari/config.lua
@@ -1,9 +1,10 @@
 Config = {}
 
-Config.name = "Safari", -- Name of the App
-Config.description = "Explore the web", -- disc in the appstore (edit for your local)
-Config.defaultApp = false, -- OPTIONAL if set to true, app should be added without having to download it,
-Config.size = 1232, -- OPTIONAL in kb
-Config.price = 0, -- OPTIONAL, Make players pay with in-game money to download the app
+Config.name = "Safari" -- Name of the App
+Config.description = "Explore the web" -- disc in the appstore (edit for your local)
+Config.defaultApp = false -- OPTIONAL if set to true, app should be added without having to download it,
+Config.size = 1232 -- OPTIONAL in kb
+Config.price = 0 -- OPTIONAL, Make players pay with in-game money to download the app
+
 
 -- Support on discord.mfpscripts.com if needed. Check out issues on github. --
